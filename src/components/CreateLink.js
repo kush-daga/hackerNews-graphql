fb6706d79197c1ee30212299b9a6ae63f50fb7b5
Kush Daga <kushdaga@hotmail.com>
2020-08-30 15:39:19 +0530

    added header and routing

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -37,7 +37,11 @@ export class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+        <Mutation
+          mutation={POST_MUTATION}
+          variables={{ description, url }}
+          onCompleted={() => this.props.history.push("/")}
+        >
           {(postMutation) => <button onClick={postMutation}>Submit</button>}
         </Mutation>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-
+import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
@@ -18,9 +18,11 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 serviceWorker.unregister();
